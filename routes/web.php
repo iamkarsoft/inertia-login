9b646b635558b30a02fc979fbe2a75d1ed34eb3f
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-03-04 00:42:39 +0000

    removed social login logic from route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\SocialLoginController;
-use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,29 +28,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
 	return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/login/github', function () {
-	return Socialite::driver('github')->redirect();
-});
-
-Route::get('/login/github/callback', function () {
-	$githubUser = Socialite::driver('github')->user();
-	// dd($user);
-	// $user->token
-	$user = User::where('provider_id', $githubUser->getId())->first();
-
-	if (!$user):
-		$user = User::create([
-			'email' => $githubUser->getEmail(),
-			'name' => $githubUser->getName(),
-			'provider_id' => $githubUser->getId(),
-		]);
-	endif;
-
-	//login user
-	auth()->login($user, true);
-	return redirect('dashboard');
-});
 
 Route::get('/login/{service}', [SocialLoginController::class, 'redirect'])->name('social.auth');
 Route::get('/login/{service}/callback', [SocialLoginController::class, 'callback'])->name('social.callback');
